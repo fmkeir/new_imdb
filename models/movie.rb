643b9a1850c4d91353fd1ac20bb9b6e60a4a8ddf
FKeir <fmkeir@gmail.com>
2020-01-30 14:20:25 +0000

    Add save to movie

--- models/movie.rb
+++ models/movie.rb
@@ -9,4 +9,14 @@ class Movie
     @genre = options["genre"]
     @budget = options["budget"].to_i
   end
+
+  def save()
+    sql = "INSERT INTO movies
+    (title, genre, budget)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@title, @genre, @budget]
+    @id = SqlRunner.run(sql, values)[0]["id"].to_i
+  end
 end
