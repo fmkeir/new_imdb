0e09b817f546c47ca8ee189b99562222b5d6bf13
FKeir <fmkeir@gmail.com>
2020-01-30 14:19:26 +0000

    Initialize movie

--- models/movie.rb
+++ models/movie.rb
@@ -1,3 +1,12 @@
 class Movie
 
+  attr_accessor :title, :genre, :budget
+  attr_reader :id
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @genre = options["genre"]
+    @budget = options["budget"].to_i
+  end
 end
